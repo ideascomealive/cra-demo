6385d400cb92a4b2703c5001bb6d338f5579a2d7
jimccoy <jimccoy@homeaway.com>
2019-07-14 13:41:03 -0500

    03 added a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Pet from './Pet/Pet';
 
 class App extends Component {
     render() {
         return (
         <div className="App">
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Hi Everybody</h1>
+            <Pet />
         </div>
         );
     }
---
+++ src/Pet/Pet.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const pet = () => {
+    return <p>I'm a cute Pet!</p>
+};
+
+export default pet;
