1631da37960b78c01c487663dbd6d2c6f7449068
ideascomealive <ideascomealive@gmail.com>
2019-07-14 12:41:06 -0500

    01 createElement example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        // return (
+        // <div className="App">
+        //   <header className="App-header">
+        //     <h1 className="App-title">Welcome to React</h1>
+        //   </header>
+        //   <p className="App-intro">
+        //     To get started, edit <code>src/App.js</code> and save to reload.
+        //   </p>
+        // </div>
+        // );
+
+        // this is a js way to create a react component - could run in the browser standalone
+        return React.createElement('div', null, 'h1', 'Hi Everybody');
+    }
 }
 
 export default App;
