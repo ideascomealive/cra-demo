947832f0631ccc375b267bc7dd7d1c9ae4d3cb03
jimccoy <jimccoy@homeaway.com>
2019-07-14 13:58:31 -0500

    05 introducing props

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
         return (
         <div className="App">
             <h1 className="App-title">Hi Everybody</h1>
-            <Pet />
-            <Pet />
-            <Pet />
+            <Pet name="Houla" age="14" />
+            <Pet name="Casey" age="12" />
+            <Pet name="Sam" age="10">I'm a ferocious beast</Pet>
         </div>
         );
     }
--- src/Pet/Pet.js
+++ src/Pet/Pet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const pet = () => {
-    return <p>I'm a cute Pet I'm {Math.floor(Math.random() * 30)} years old.</p>
+const pet = (props) => {
+    return <p>My name is {props.name} and I'm {props.age} years old.</p>
 };
 
 export default pet;
