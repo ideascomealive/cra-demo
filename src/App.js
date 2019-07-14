977c6d5411cc4bb28b749b72d82432ffb6cf2fa6
jimccoy <jimccoy@homeaway.com>
2019-07-14 13:22:22 -0500

    02 back to JSX

--- src/App.js
+++ src/App.js
@@ -3,19 +3,11 @@ import './App.css';
 
 class App extends Component {
     render() {
-        // return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <h1 className="App-title">Welcome to React</h1>
-        //   </header>
-        //   <p className="App-intro">
-        //     To get started, edit <code>src/App.js</code> and save to reload.
-        //   </p>
-        // </div>
-        // );
-
-        // this is a js way to create a react component - could run in the browser standalone
-        return React.createElement('div', null, 'h1', 'Hi Everybody');
+        return (
+        <div className="App">
+            <h1 className="App-title">Welcome to React</h1>
+        </div>
+        );
     }
 }
 
