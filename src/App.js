6c10c4f32d7a146f8b985600ea47a373c8c6e4e8
jimccoy <jimccoy@homeaway.com>
2019-07-14 22:22:09 -0500

    07 introducing state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Pet from './Pet/Pet';
 
 class App extends Component {
+    state = {
+        pets: [
+            { name: 'Houla', age: 14 },
+            { name: 'Casey', age: 12 },
+            { name: 'Sam', age: 10 }            
+        ]
+    }
+
     render() {
         return (
         <div className="App">
             <h1 className="App-title">Hi Everybody</h1>
-            <Pet name="Houla" age="14" />
-            <Pet name="Casey" age="12" />
-            <Pet name="Sam" age="10">I'm a ferocious beast</Pet>
+            <button>Switch Pet</button>
+            <Pet name={this.state.pets[0].name} age={this.state.pets[0].age} />
+            <Pet name={this.state.pets[1].name} age={this.state.pets[1].age} />
+            <Pet name={this.state.pets[2].name} age={this.state.pets[2].age}>I'm a ferocious beast</Pet>
         </div>
         );
     }
