4b05c10db66289f6fa4f6f2d9d8b9611c6c4306e
jimccoy <jimccoy@homeaway.com>
2019-07-15 01:54:46 -0500

    11 passing methods as props

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
 //import useState instead of component
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Pet from './Pet/Pet';
 
-//change class to function
-const App = props => {
-    //create initial state using useState and array destructuring
-   const [petState, setPetState] = useState({
-    pets: [
-        { name: 'Houla', age: 14 },
-        { name: 'Casey', age: 12 },
-        { name: 'Sam', age: 10 }            
-    ],
-    //if I had additional state it will get lost with useState
-    //when switchPetHandler is called this value will be lost
-    moreState: 'some other value'
-   });
-
-   //add the handler as a function inside a function
-   const switchPetHandler = () => {
-   //call the updater function to change the state    
-    setPetState({
+class App extends Component {
+    state = {
         pets: [
-            { name: 'Chance', age: 4 },
-            { name: 'Sassy', age: 14 },
-            { name: 'Shadow', age: 14 }            
+            { name: 'Houla', age: 14 },
+            { name: 'Casey', age: 12 },
+            { name: 'Sam', age: 10 }            
         ]
-    });
-   };
+    }
+    
+    switchPetHandler = () => {
+        //never mutate state directly example: `this.state.pets = []` will not work
+        this.setState({
+            pets: [
+                { name: 'Chance', age: 4 },
+                { name: 'Sassy', age: 14 },
+                { name: 'Shadow', age: 14 }            
+            ]
+        })
+    }
 
-   //functional components only need to return jsx
-   //we change the way we tie the element state value
-    return (
+    //we add a reference to the method in onClick
+    render() {
+        return (
         <div className="App">
             <h1 className="App-title">Hi Everybody</h1>
-            <button onClick={switchPetHandler}>Switch Pet</button>
-            <Pet name={petState.pets[0].name} age={petState.pets[0].age} />
-            <Pet name={petState.pets[1].name} age={petState.pets[1].age} />
-            <Pet name={petState.pets[2].name} age={petState.pets[2].age}>I'm a ferocious beast</Pet>
+            <button onClick={this.switchPetHandler}>Switch Pet</button>
+            <Pet name={this.state.pets[0].name} age={this.state.pets[0].age} />
+            <Pet name={this.state.pets[1].name} age={this.state.pets[1].age} />
+            <Pet name={this.state.pets[2].name} age={this.state.pets[2].age} click={this.switchPetHandler} >I am ferocious beast</Pet>
         </div>
-    );
+        );
+    }
 }
 
 export default App;
