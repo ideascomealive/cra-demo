a70fdbd826be73d9f9453815b64b763c221204d8
jimccoy <jimccoy@homeaway.com>
2019-07-14 22:13:05 -0500

    06 the children property

--- src/Pet/Pet.js
+++ src/Pet/Pet.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const pet = (props) => {
-    return <p>My name is {props.name} and I'm {props.age} years old.</p>
+    return (
+      <div>
+        <p>My name is {props.name} and I'm {props.age} years old.</p>
+        <p>{props.children}</p>
+      </div>
+    )
 };
 
 export default pet;
