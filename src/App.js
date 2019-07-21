dade2dde799660f3452ab294f983cbbbaccd7dc3
jimccoy <jimccoy@homeaway.com>
2019-07-21 18:00:59 -0500

    16 conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
             { name: 'Houla', age: 14 },
             { name: 'Casey', age: 12 },
             { name: 'Sam', age: 10 }            
-        ]
+        ],
+        showPets: false //hide/show boolean
     }
     
     switchPetHandler = (newName) => {
-        //never mutate state directly example: `this.state.pets = []` will not work
         this.setState({
             pets: [
                 { name: newName, age: 4 },
@@ -32,9 +32,15 @@ class App extends Component {
             ]
         })
     }
+          
+    //Using => arrow syntax binds `this`
+    togglePetsHandler = () => {
+      const show = this.state.showPets;
+      this.setState({showPets: !show});
+    }
+    //with setState, state merges
 
     render() {
-        // we add styles as a js object
         const styles = {
             backgroundColor: 'white',
             font: 'inherit',
@@ -43,21 +49,24 @@ class App extends Component {
             cursor: 'pointer'
         };
 
-        // we add a reference inline to the style we defined above
         return (
         <div className="App">
             <h1 className="App-title">Hi Everybody</h1>
             <button 
                 style={styles}
-                onClick={this.switchPetHandler.bind(this, 'Snarf')}>Switch Pet</button>
-            <Pet name={this.state.pets[0].name} 
-                age={this.state.pets[0].age} />
-            <Pet name={this.state.pets[1].name} 
-                age={this.state.pets[1].age} />
-            <Pet name={this.state.pets[2].name} 
-                age={this.state.pets[2].age} 
-                click={this.switchPetHandler.bind(this, 'Lionel')}
-                changed={this.petChangeHandler} >I am ferocious beast</Pet>
+                onClick={this.togglePetsHandler}>Switch Pet</button>
+          {this.state.showPets ? 
+            <div>
+              <Pet name={this.state.pets[0].name} 
+                  age={this.state.pets[0].age} />
+              <Pet name={this.state.pets[1].name} 
+                  age={this.state.pets[1].age} />
+              <Pet name={this.state.pets[2].name} 
+                  age={this.state.pets[2].age} 
+                  click={this.switchPetHandler.bind(this, 'Lionel')}
+                  changed={this.petChangeHandler} >I am ferocious beast</Pet>
+            </div> : null
+          }
         </div>
         );
     }
