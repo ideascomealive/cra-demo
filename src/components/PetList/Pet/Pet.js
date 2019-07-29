bc05114d0cb6833fea0cf988a42681b69052aa84
jimccoy <jimccoy@homeaway.com>
2019-07-29 16:52:28 -0500

    42 using React.createRef

--- src/components/PetList/Pet/Pet.js
+++ src/components/PetList/Pet/Pet.js
@@ -3,11 +3,17 @@ import Wrapper from '../../../hoc/Wrapper';
 import './Pet.css';
 
 class Pet extends Component {
+  //add constructor to create a ref
+  constructor(props) {
+    super();
+    //React.createRef
+    this.inputElementRef = React.createRef();
+  }
 
-  //added componentDidMount() with reference
+  //reference from constructor and React.createRe(); - inputElementRef
   componentDidMount() {
-    //inputElement will be set by the time componentDidMount runs
-    this.inputElement.focus();
+    //add .current for current element
+    this.inputElementRef.current.focus();
   }
   render() {
   console.log('Child Components rendering')
@@ -15,10 +21,10 @@ class Pet extends Component {
       <Wrapper>
         <p onClick={this.props.click}>My name is {this.props.name} and I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
-        {/* Set an internal property called this.inputElement */}
         <input 
         type="text" 
-        ref={(inputEl) => {this.inputElement = inputEl}}
+        // ref={(inputEl) => {this.inputElement = inputEl}} //removed other ref
+        ref={this.inputElementRef}
         onChange={this.props.changed} 
         value={this.props.name} />
       </Wrapper>
