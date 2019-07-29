9392d0b37df1634930e8758dce805b5ca6fa7ec9
jimccoy <jimccoy@homeaway.com>
2019-07-29 14:30:41 -0500

    39 HOC wrapper

--- src/components/PetList/Pet/Pet.js
+++ src/components/PetList/Pet/Pet.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+//import Wrapper component
+import Wrapper from '../../../hoc/Wrapper';
 import './Pet.css';
 
 class Pet extends Component {
   render() {
   console.log('Child Components rendering')
     return (
-      <div className="Pet">
+      <Wrapper>
+        {/*Wrap everything in a HOC instead of a div*/}
         <p onClick={this.props.click}>My name is {this.props.name} and I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Wrapper>
     );
   }
 }
---
+++ src/hoc/Wrapper.js
@@ -0,0 +1,6 @@
+//we don't need an import of react because we are not using any jsx
+
+//all we do is return the children
+const Wrapper = props => props.children;
+
+export default Wrapper;
